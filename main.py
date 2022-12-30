d8d5e728f4d0da11462f06c37f5a53468a28be50
Eggier <105437746+Eggier@users.noreply.github.com>
2022-12-31 01:11:00 +0500

    3.2.(1,2,3)

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import threading
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class DataSet:
+    def __init__(self, file_name):
+        self.data = pd.read_csv(file_name)
+
+    def makePartsCsv(data: pd.DataFrame, file_name):
+        data.to_csv(path_or_buf=file_name, index=False, encoding='utf-8-sig')
+
+    def makeResult(self):
+        for year in self.data['published_at'].apply(lambda x: x[:4]).unique():
+            filter_parametr = self.data['published_at'].str.contains(year)
+            filtered_data = self.data[filter_parametr]
+            t = threading.Thread(target=makePartsCsv, args=(filtered_data, f'split_files/vacancies_by_{year}.csv'))
+            t.start()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = DataSet('vacancies_by_year.csv')
+data.makeResult()
